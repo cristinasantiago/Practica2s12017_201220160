fc15b286a7afe2d30932239c1a76feb0bed5ec7d
cristinasan58 <cristinasan58@gmail.com>
2017-03-06 00:41:36 -0600

    ya casi
    ya casi

--- Practica2EDD_201220160/src/practica2edd_201220160/TestWebServer.java
+++ Practica2EDD_201220160/src/practica2edd_201220160/TestWebServer.java
@@ -5,17 +5,149 @@
  */
 package practica2edd_201220160;
 
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+
 /**
  *
  * @author Cristina
  */
 public class TestWebServer {
 
+    public static OkHttpClient webClient = new OkHttpClient();
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String nombre = "Marco";
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", nombre)
+                .add("dato2", "4")
+                .build();
+        String r = getString("metodoWeb", formBody); 
+        System.out.println(r + "---");
+    }
+    public static void nombre(String nombre)
+    {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", nombre)
+                .add("dato2", "4")
+                .build();
+        String r = getString("metodoWeb", formBody); 
+        System.out.println(r + "---");
+    }
+    
+    public static void insertarLista(String Valor)
+    {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", Valor)
+                .build();
+        String r = getString("agregarLista", formBody); 
+        System.out.println(r);
+    }
+    public static void buscarLista(String Valor)
+    {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", Valor)
+                .build();
+        String r = getString("BuscarLista", formBody); 
+        System.out.println(r);
+    }
+    public static void EliminarLista(String Valor)
+    {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", Valor)
+                .build();
+        String r = getString("EliminarLista", formBody); 
+        System.out.println(r);
+    }
+       public static void Push(String Valor)
+    {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", Valor)
+                .build();
+        String r = getString("Push", formBody); 
+        System.out.println(r);
+    }
+          public static void Queue(String Valor)
+    {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", Valor)
+                .build();
+        String r = getString("Queue", formBody); 
+        System.out.println(r);
+    }
+          public static void Pop(String Valor)
+    {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", Valor)
+                .build();
+        String r = getString("Pop", formBody); 
+        System.out.println(r);
+    }          
+     
+          public static void Dequeue(String Valor)
+    {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", Valor)
+                .build();
+        String r = getString("Dequeue", formBody); 
+        System.out.println(r);
+    }    
+          public static void InsertarMatriz(String Valor)
+    {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", Valor)
+                .build();
+        String r = getString("InsertarMatriz", formBody); 
+        System.out.println(r);
+    }  
+          public static void BuscarLetra(String Valor)
+    {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", Valor)
+                .build();
+        String r = getString("BuscarLetra", formBody); 
+        System.out.println(r);
+    }  
+            public static void EliminarMatriz(String Valor)
+    {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", Valor)
+                .build();
+        String r = getString("EliminarMatriz", formBody); 
+        System.out.println(r);
+    }  
+             public static void BuscarDominio(String Valor)
+    {
+        RequestBody formBody = new FormEncodingBuilder()
+                .add("dato", Valor)
+                .build();
+        String r = getString("BuscarDominio", formBody); 
+        System.out.println(r);
+    }  
+     public static String getString(String metodo, RequestBody formBody) {
+
+        try {
+            URL url = new URL("http://0.0.0.0:5000/" + metodo);
+            Request request = new Request.Builder().url(url).post(formBody).build();
+            Response response = webClient.newCall(request).execute();//Aqui obtiene la respuesta en dado caso si hayas pues un return en python
+            String response_string = response.body().string();//y este seria el string de las respuesta
+            return response_string;
+        } catch (MalformedURLException ex) {
+            java.util.logging.Logger.getLogger(TestWebServer.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(TestWebServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
     
 }
